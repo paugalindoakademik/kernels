9aab08c030f6a5826e1fea49064653ab8bd051b8
paugalindoakademik <pau.galindo.figuerola@estudiantat.upc.edu>
2024-05-01 22:28:11 +0200

    deleted:    cuda_2023/DIRECTORIES

--- cuda_2023/benchmarks/histogram/src/cuda/kernel.h
+++
@@ -1,4 +0,0 @@
-#define MAX_VAL 256
-
-__global__ void histo_simple(unsigned char *buffer, long size, unsigned *histo);
-__global__ void histo_privatization(unsigned char *buffer, long size, unsigned *histo);
